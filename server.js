2b4500afb36c439dfb327bad98d3e04c796da001
blakflagg <dduffy2@outlook.com>
2016-05-18 20:29:03 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+var app = express();
+const PORT = 3000;
+// app.get('/', function(req,res){
+//     res.send('Hello Express');
+// });
+
+var middleware = {
+    requireAuthentication: function(req,res,next){
+        console.log('private route hit');
+        next();
+    },
+    logger: function(req,res,next){
+        console.log(req.method + ' ' + req.originalUrl + 'Date: ' + new Date().toString());
+        next();
+    }
+};
+
+app.use(middleware.logger);
+
+app.get('/about',middleware.requireAuthentication,function(req,res){
+    res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function(){
+    console.log("Express server started on port " + PORT);
+    
+});
+
